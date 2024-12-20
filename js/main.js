3fb6f573b5c540b97b1eb031a76ee35c780b22ba
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-20 11:34:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,32 +7,38 @@ document.querySelectorAll('.navbar__language').forEach((languageSelector) => {
     LanguageToggler.addEventListener('click', (event) => {
         event.preventDefault();
 
+        // Check if the current languageSelector is active
+        const isActive = languageSelector.classList.contains('navbar__language--active');
+
+        // Reset all other open blocks
         document.querySelectorAll('.navbar__language').forEach((otherSelector) => {
             const otherList = otherSelector.querySelector('.navbar__language-list');
             otherSelector.classList.remove('navbar__language--active');
-            otherList.removeAttribute('style');
+            otherList.removeAttribute('style'); // Remove height
         });
 
-        if (!languageSelector.classList.contains('navbar__language--active')) {
+        // If the current one was inactive, activate it
+        if (!isActive) {
             languageSelector.classList.add('navbar__language--active');
-            languageList.style.maxHeight = languageList.scrollHeight + 'px';
+            languageList.style.maxHeight = languageList.scrollHeight + 'px'; // Set height
         } else {
+            // If it was active, deactivate it
             languageSelector.classList.remove('navbar__language--active');
-            languageList.removeAttribute('style');
+            languageList.removeAttribute('style'); // Remove height
         }
     });
 });
 
+// Close all lists when clicking outside the blocks
 document.addEventListener('click', (event) => {
     document.querySelectorAll('.navbar__language').forEach((languageSelector) => {
         if (!languageSelector.contains(event.target)) {
             const languageList = languageSelector.querySelector('.navbar__language-list');
             languageSelector.classList.remove('navbar__language--active');
-            languageList.removeAttribute('style');
+            languageList.removeAttribute('style'); // Remove height
         }
     });
 });
-
 
 // =============== TABS ===============
 
