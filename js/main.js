7600c49ebfca8b52d79dc68898d418e0fdcd6d1d
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-20 10:05:11 +0200

    +

--- js/main.js
+++ js/main.js
@@ -7,38 +7,32 @@ document.querySelectorAll('.navbar__language').forEach((languageSelector) => {
     LanguageToggler.addEventListener('click', (event) => {
         event.preventDefault();
 
-        // Check if the current languageSelector is active
-        const isActive = languageSelector.classList.contains('navbar__language--active');
-
-        // Reset all other open blocks
         document.querySelectorAll('.navbar__language').forEach((otherSelector) => {
             const otherList = otherSelector.querySelector('.navbar__language-list');
             otherSelector.classList.remove('navbar__language--active');
-            otherList.removeAttribute('style'); // Removing the height
+            otherList.removeAttribute('style');
         });
 
-        // If the current one was inactive, activate it
-        if (!isActive) {
+        if (!languageSelector.classList.contains('navbar__language--active')) {
             languageSelector.classList.add('navbar__language--active');
-            languageList.style.maxHeight = languageList.scrollHeight + 'px'; // Setting the height
+            languageList.style.maxHeight = languageList.scrollHeight + 'px';
         } else {
-            // If it was active, deactivate it
             languageSelector.classList.remove('navbar__language--active');
-            languageList.removeAttribute('style'); // Removing the height
+            languageList.removeAttribute('style');
         }
     });
 });
 
-// Close all lists when clicking outside the blocks
 document.addEventListener('click', (event) => {
     document.querySelectorAll('.navbar__language').forEach((languageSelector) => {
         if (!languageSelector.contains(event.target)) {
             const languageList = languageSelector.querySelector('.navbar__language-list');
             languageSelector.classList.remove('navbar__language--active');
-            languageList.removeAttribute('style'); // Removing the height
+            languageList.removeAttribute('style');
         }
     });
 });
+
 
 // =============== TABS ===============
 
@@ -47,15 +41,13 @@ const tabContents = document.querySelectorAll('.tabs-content');
 
 tabBtns.forEach(tabBtn => {
     tabBtn.addEventListener('click', () => {
-        const target = tabBtn.getAttribute('data-tab-target');
+        const target = tabBtn.getAttribute('data-tabs-target');
 
-        // Remove the active class from all tabsBtns and tabContents
         tabBtns.forEach(tabBtn => tabBtn.classList.remove('tabs-btn--active'));
         tabContents.forEach(tabContent => tabContent.classList.remove('tabs-content--active'));
 
-        // Add an active class for the selected tabBtn and tabContents
         tabBtn.classList.add('tabs-btn--active');
-        document.querySelectorAll(`.tabs-content[data-tab-content="${target}"]`).forEach(tabContent => {
+        document.querySelectorAll(`.tabs-content[data-tabs-content="${target}"]`).forEach(tabContent => {
             tabContent.classList.add('tabs-content--active');
         });
     });
